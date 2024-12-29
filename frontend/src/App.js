349e6a83e2206e7195b688c093f96be8018b5d1b
Raghavihinge <hraghavi888@gmail.com>
2024-12-29 18:42:04 +0530

    Added Fronted code for fetching and displaying news

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import News from './components/News';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <News/>
+    </>
+  );
+}
+
+export default App;
